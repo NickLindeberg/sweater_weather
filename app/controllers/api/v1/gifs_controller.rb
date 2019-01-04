3509bb983574280e1fa0270a31f7d25bdddf2a6e
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-04 10:02:37 -0700

    setting up google coords with darksky in giphy controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::GifsController < ApplicationController
 
   def show
+    require "pry"; binding.pry
+    googe_data = GoogleCoordinateService.new
+    coords = googe_data.get_coordinates(params[:location])
+    darksky = DarkskyService.new
+    weather = darksky.get_city_forecast(coords[:lat], coords[:lng])
     gifs = GiphyService.new
-    # coords = googe_data.get_coordinates(params[:location])
-    # darksky = DarkskyService.new
-    # weather = darksky.get_city_forecast(coords[:lat], coords[:lng])
-    # render json: weather
+    # gif = gifs.get_gif(params[:])
   end
 
 end
