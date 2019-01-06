4a74c162a1ffed6a00c2e7ef8b60fe03759acbc9
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-06 13:57:24 -0700

    set up basic logic for session create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-
+    session_user = User.find_by(email: params[:email])
+    if session_user
+      render json: "User Created"
+    else
+      render json: "Login Failed", status: 401
+    end
   end
 
 end
