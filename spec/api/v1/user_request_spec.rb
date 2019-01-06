b3dc137806508a521ec5b8bdc567a34d05f05384
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-06 14:16:13 -0700

    add serializer and sad path test

--- spec/api/v1/user_request_spec.rb
+++ spec/api/v1/user_request_spec.rb
@@ -31,8 +31,20 @@ describe 'request' do
     post "/api/v1/sessions", params: user_data, headers: headers
 
     expect(response).to be_successful
-require "pry"; binding.pry
-    response_hash = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(200)
 
+    response_hash = JSON.parse(response.body, symbolize_names: true)
+    expect(response_hash[:data][:attributes]).to include(:api_key)
+  end
+
+  it 'returns 401 if email is incorrect' do
+    user = User.create(email: "nick@nick.com", password: "2345", api_key: "6780")
+    headers = { "Content_Type" => "application/json", "Accept" => "application/json" }
+    user_data = { email: "nick@rick.com", password: user.password}
+
+    post "/api/v1/sessions", params: user_data, headers: headers
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
   end
 end
