2fc3917738d415d6d36f617f6634b0f9ba6202fd
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-08 14:17:06 -0700

    creates weather adapter poro and moves service logic out of forcast builder

--- app/models/forecast_builder.rb
+++ app/models/forecast_builder.rb
@@ -1,11 +1,15 @@
 class ForecastBuilder
 
+  def initialize
+    @weather_adapter = WeatherAdapter.new
+  end
+
+
   def build_forecast(location)
     googe_data = GoogleCoordinateService.new
     coords = googe_data.get_coordinates(location)
 
-    darksky = DarkskyService.new
-    weather = darksky.get_city_forecast(coords[:lat], coords[:lng])
+    weather = @weather_adapter.build(coords)
 
     giphy = GiphyService.new
     giphy_url = {"giphy_url": giphy.get_gif_url(weather[:currently][:summary])}
@@ -14,5 +18,4 @@ class ForecastBuilder
     weather_response.merge!(weather)
     weather_response.merge!(giphy_url)
   end
-
 end
