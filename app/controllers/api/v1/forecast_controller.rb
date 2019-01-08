d5a35db8ab94d9655e75c11b71a374f5f38cbeeb
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-08 09:50:43 -0700

    moved logic out of the controller into the poro for forecast;

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,17 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
-    googe_data = GoogleCoordinateService.new
-    coords = googe_data.get_coordinates(params[:location])
-    darksky = DarkskyService.new
-    weather = darksky.get_city_forecast(coords[:lat], coords[:lng])
-
-    giphy = GiphyService.new
-    giphy_url = {"giphy_url": giphy.get_gif_url(weather[:currently][:summary])}
-    weather_response = Hash.new()
-    weather_response.merge!(weather)
-    weather_response.merge!(giphy_url)
-
+    builder = ForecastBuilder.new
+    weather_response = builder.build_forecast(params[:location])
+    
     render json: weather_response
   end
 
